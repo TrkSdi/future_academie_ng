59d8232fa6f49cbda22d5120ce16640f17cf5b38
Pierrick-LB <pierrick.lb2003@gmail.com>
2024-02-02 11:57:19 +0100

    add studyProgramDetail component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
 import { SchoolsComponent } from './schools/schools.component';
+import { StudydetailComponent } from './studydetail/studydetail.component';
 
-export const routes: Routes = [{ path: "school/:id", component: SchoolsComponent },];
+export const routes: Routes = [{ path: "school/:id", component: SchoolsComponent }, {path: "studyprogram/:id", component:StudydetailComponent}];
