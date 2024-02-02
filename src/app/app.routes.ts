8236eaa6ff45d2468b135cf19033ae3419172ba1
Damien Vialla de Soleyrol <damienvialla@gmail.com>
2024-02-02 15:49:48 +0100

    component creation account

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { SchoolsComponent } from './schools/schools.component';
+import { CreationComponent } from './creation/creation.component';
 
-export const routes: Routes = [{ path: "school/:id", component: SchoolsComponent },];
+export const routes: Routes = [
+  { path: 'school/:id', component: SchoolsComponent },
+  { path: 'creation', component: CreationComponent },
+];
