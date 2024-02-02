9910a84af419a5d0ccc5836a7d800fe71376a4a7
Gilles Helleu <helleugilles@gmail.com>
2024-02-02 12:25:42 +0100

    add search

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { SchoolsComponent } from './schools/schools.component';
+import { SearchComponent } from './search/search.component';
 
-export const routes: Routes = [{ path: "school/:id", component: SchoolsComponent },];
+export const routes: Routes = [
+  { path: 'school/:id', component: SchoolsComponent },
+  { path: 'search/', component: SearchComponent },
+];
