b64c553b2fdf5ab7be67b0081f646471010d3eb4
Rachel W. <rachelwilliams@mailbox.org>
2024-02-03 14:46:22 +0100

    starting favorite form

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -12,12 +12,15 @@ import { FavoriteService } from '../services/favorite.service';
 export class FavoriteComponent {
   constructor(private user: UserService, private favService: FavoriteService) { }
 
-  addFavorite(program_id: number, status: string) {
+  addFavorite(program_id: number) {
     const user_id: string | null = this.user.getUserID();
     if (user_id) {
-      this.favService.createFavorite(program_id, status, user_id).subscribe({
+      this.favService.createFavorite(program_id, user_id).subscribe({
         next: (results) => {
           console.log(results); // this is temporary will add real directions later
+          // idea is this function is used by clicking "add favorite" on a program
+          // and then it opens an individual favorite page where you 
+          //can add a status or a note and save it
         }
       });
     }
