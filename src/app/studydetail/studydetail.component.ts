6785a9b3dd44eafcbdcf3841360538fe28fcbf21
Rachel W. <rachelwilliams@mailbox.org>
2024-02-04 14:35:12 +0100

    added save favorite button

--- src/app/studydetail/studydetail.component.ts
+++ src/app/studydetail/studydetail.component.ts
@@ -2,25 +2,33 @@ import { Component, Input } from '@angular/core';
 import { StudyProgram } from '../interface/study-interface';
 import { StudyProgramDetailService } from '../services/studydetail.service';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { UserService } from '../services/user.service';
+import { FavoriteService } from '../services/favorite.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-studydetail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './studydetail.component.html',
   styleUrl: './studydetail.component.css'
 })
 
 export class StudydetailComponent {
 
-  constructor(private studyprogramdetailService: StudyProgramDetailService, private route: ActivatedRoute) { }
+  constructor(private studyprogramdetailService: StudyProgramDetailService, private route: ActivatedRoute, private router: Router, private auth: AuthService, private user: UserService, private favoriteService: FavoriteService) { }
+  loginMessage: string = '';
 
+  // Needed to next study-detail in favorite view
   @Input() studyProgram: StudyProgram | null = null;
+  currentRoute: string = this.route.toString();
 
   ngOnInit() {
     this.loadStudyProgram();
-    console.log(this.studyProgram)
   }
+
   loadStudyProgram() {
     const id = this.route.snapshot.paramMap.get('id'); {
       this.studyprogramdetailService.getStudyProgram(id).subscribe({
@@ -31,5 +39,25 @@ export class StudydetailComponent {
         }
       });
     }
+  }
+
+  saveFavorite() {
+    const program_id = this.studyProgram?.cod_aff_form
+    if (this.auth.isAuthenticated()) {
+      const user_id = this.user.getUserID()
+      this.favoriteService.createFavorite(program_id!, user_id!).subscribe({
+        next: (response) => {
+          this.router.navigateByUrl(`/favorite/${response.id}`);
+        }
+      });
+    } else {
+      this.loginMessage = "Connectez-vous pour sauvegarder des favoris!"
+      setTimeout(() => {
+        this.loginMessage = ""
+        // window.location.reload();
+      }, 4000);
+
+    }
+
   }
 }
