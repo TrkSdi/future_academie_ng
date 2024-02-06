30dd525a900a4da7e57d7840693de2e67c56f599
Pierrick-LB <pierrick.lb2003@gmail.com>
2024-02-06 16:13:30 +0100

    userprofile update

--- src/app/services/userprofile.service.ts
+++ src/app/services/userprofile.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { UserProfile } from '../interface/userprofile-interface';
 import { map } from 'rxjs';
 import { ApiconfigService } from './apiconfig.service';
@@ -13,24 +13,25 @@ export class UserProfileService {
 
   constructor(private http: HttpClient, private api: ApiconfigService) { }
 
-  url: string = this.api.getAPIUrl() + "/API_public/userprofile";
+  url: string = this.api.getAPIUrl() + "/API_private/userprofile";
 
   getUserProfile(): Observable<UserProfile> {
     return this.http.get<UserProfile>(this.url).pipe(
+      tap ((response: any) => (console.log(response))),
       map((response: any) => ({
        
-        username: response.username,
-        first_name: response.first_name,
-        last_name: response.last_name,
-        email: response.email,
+        username: response.results.user_extended ? response.user_extended.username : null,
+        first_name: response.results.user_extended ? response.user_extended.first_name : null,
+        last_name: response.results.user_extended ? response.user_extended.last_name : null,
+        email: response.results.user_extended ? response.user_extended.email : null,
 
 
-      image_profile: response.image_profile,
-      url_tiktok: response.url_tiktok_extended ? response.url_tiktok_extended.link_url : null,
-      url_instagram: response.url_instagram_extended ? response.url_instagram_extended.link_url : null,
-      about_me: response.about_me,
-      is_public: response.is_public,
-      student_at: response.student_at
+      image_profile: response.results.image_profile,
+      url_tiktok: response.results.url_tiktok_extended ? response.url_tiktok_extended.link_url : null,
+      url_instagram: response.results.url_instagram_extended ? response.url_instagram_extended.link_url : null,
+      about_me: response.results.about_me,
+      is_public: response.results.is_public,
+      student_at: response.results.student_at
     })))
   }
 }
