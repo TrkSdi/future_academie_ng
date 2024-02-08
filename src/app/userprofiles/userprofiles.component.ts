a48eeac1e8fcf82bb25fd5686b0610b1b774950d
Pierrick-LB <pierrick.lb2003@gmail.com>
2024-02-08 14:28:45 +0100

    add feature edit on about_me for userprofile

--- src/app/userprofiles/userprofiles.component.ts
+++ src/app/userprofiles/userprofiles.component.ts
@@ -4,72 +4,26 @@ import { ActivatedRoute } from '@angular/router';
 import { UserProfileService } from '../services/userprofile.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
+
 
 
 @Component({
   selector: 'app-userprofiles',
   standalone: true,
   imports: [CommonModule, FormsModule],
-  // imports: [CommonModule, FormsModule, NgModule],
   templateUrl: './userprofiles.component.html',
   styleUrl: './userprofiles.component.css'
 })
-// export class UserProfilesComponent implements OnInit {
-
-//   userprofile!: UserProfile;
-//   aboutMeText: string = '';
-
-//   constructor(private userprofileService: UserProfileService, private route: ActivatedRoute) { }
-
-//   ngOnInit() {
-//     this.loadUserProfile();
-//   }
-
-//   loadUserProfile() {
-//     this.userprofileService.getUserProfile().subscribe({
-//       next: (userprofile: UserProfile) => {
-//         this.userprofile = userprofile;
-//         this.aboutMeText = userprofile.about_me || '';
-//         console.log('User Profile Data:', userprofile); 
-//       },
-//       error: (error) => {
-//         console.error('Error fetching user profile:', error);
-//       }
-//     });
-//   }
-
-//   updateAboutMe() {
-//     this.userprofile.about_me = this.aboutMeText;
-//     this.userprofileService.updateUserProfile(this.userprofile).subscribe({
-//       next: () => {
-//         console.log('User profile updated successfully');
-//       },
-//       error: (error) => {
-//         console.error('Error updating user profile:', error);
-//       }
-//     });
-//   }
-
-  
-//   updateAboutMeText(event: any) {
-//     this.aboutMeText = event.target.value;
-//   }
-// }
-
-
 
 export class UserProfilesComponent implements OnInit {
-
   userprofile!: UserProfile;
   aboutMeText: string = '';
   editingMode: boolean = false; 
 
-  constructor(private userprofileService: UserProfileService, private route: ActivatedRoute) { }
+  constructor(private userprofileService: UserProfileService) { }
 
   ngOnInit() {
-    this.loadUserProfile();
-    
+    this.loadUserProfile();   
   }
 
   loadUserProfile() {
@@ -78,22 +32,9 @@ export class UserProfilesComponent implements OnInit {
         this.userprofile = userprofile;
         this.aboutMeText = userprofile.about_me || '';
         console.log('User Profile Data:', userprofile);
-        this.updateAboutMe(); 
       },
       error: (error) => {
         console.error('Error fetching user profile:', error);
-      }
-    });
-  }
-
-  updateAboutMe() {
-    this.userprofile.about_me = "string";
-    this.userprofileService.updateUserProfile(this.userprofile).subscribe({
-      next: (results) => {
-        console.log('User profile updated successfully', results);
-      },
-      error: (error) => {
-        console.error('Error updating user profile:', error);
       }
     });
   }
@@ -105,5 +46,20 @@ export class UserProfilesComponent implements OnInit {
   cancelEditing() {
     this.editingMode = false;
     this.aboutMeText = this.userprofile.about_me || '';
+  }
+
+  saveAboutMe() {
+    this.userprofile.about_me = this.aboutMeText;
+    this.userprofileService.updateUserProfile(this.userprofile).subscribe({
+      next: (results) => {
+        console.log('User profile updated successfully', results);
+        this.editingMode = false; 
+      },
+      error: (error) => {
+        console.error('Error updating user profile:', error);
+        
+        this.editingMode = true;
+      }
+    });
   }
 }
